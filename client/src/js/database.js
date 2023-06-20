9351940a719c0fea3590255a3e335806ff982f8f
unknown <jayssendecastro@gmail.com>
2023-06-20 20:28:59 +0100

    fixed database error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ text: text });
+  const request = store.add({ text: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
